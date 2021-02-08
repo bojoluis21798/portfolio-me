075b6e75f19f97b9cb05cfc380fad982fa560268
bojoluis21798 <bojoluis21798@gmail.com>
2021-02-08 16:26:17 +0800

    Create on submit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles/main.scss";
+
+const $ = require("jquery");
+//const nodemailer = require("nodemailer");
+
+$("#email-form").submit(function (event) {
+  event.preventDefault();
+  const formData = $("#email-form").serializeArray();
+});
