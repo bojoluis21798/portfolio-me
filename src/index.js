81fd03dc8e0079bfdbcf83cabe29373891668df8
bojoluis21798 <bojoluis21798@gmail.com>
2021-02-09 17:36:35 +0800

    Make sending state for email form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import "./styles/main.scss";
 
 const $ = require("jquery");
-//const nodemailer = require("nodemailer");
 
-const emailForm = $("#email-form");
-
-emailForm.submit(function (event) {
+// Contact us send to my email
+$("#email-form").submit(function (event) {
   event.preventDefault();
 
-  const formData = emailForm.serializeArray();
-  emailForm.trigger("reset");
+  const formData = $(this).serializeArray();
+
+  const sendButton = $("#send");
+  const form = [$("#name"), $("#email"), $("#message"), sendButton];
+
+  // SENDING STATUS
+  form.forEach((f) => f.prop("disabled", true));
+  sendButton.toggleClass("sending");
+  sendButton.val("Sending");
+
+  new Promise((r) => setTimeout(r, 1000))
+    .then(() => {
+      // SENT STATUS
+      form.forEach((f) => f.prop("disabled", false));
+      sendButton.toggleClass("sending");
+      sendButton.toggleClass("sent");
+      sendButton.val("Sent");
+
+      $(this).trigger("reset");
+    })
+    .then(() => new Promise((r) => setTimeout(r, 3000)))
+    .then(() => {
+      // RESET STATUS
+      sendButton.val("Send");
+      sendButton.toggleClass("sent");
+    });
 });
