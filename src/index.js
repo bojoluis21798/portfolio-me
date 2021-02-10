00dd25452c1acc5fa0017d56990f104bb0dd2358
bojoluis21798 <bojoluis21798@gmail.com>
2021-02-10 14:32:32 +0800

    Modulate scripts

--- src/index.js
+++ src/index.js
@@ -1,57 +1,12 @@
+import $ from "jquery";
 import "./styles/main.scss";
-
-const $ = require("jquery");
-const emailjs = require("emailjs-com");
-
-const config = require("../config/credentials.json");
+import contactFormSubmit from "./scripts/contactFormSubmit";
+import smoothScroll from "./scripts/smoothScroll";
 
 $(function () {
   const breadcrumb = $("#breadcrumb");
   breadcrumb.hide();
 
-  // Contact us send to my email
-  $("#email-form").submit(function (event) {
-    event.preventDefault();
-
-    const user_name = $("#name");
-    const user_email = $("#email");
-    const message = $("#message");
-    const form = [user_name, user_email, message];
-
-    // SENDING STATUS
-    form.forEach((f) => f.prop("disabled", true));
-
-    const formData = {
-      user_name: user_name.val(),
-      user_email: user_email.val(),
-      message: message.val(),
-    };
-
-    emailjs
-      .send(config.service_id, config.template_id, formData, config.user_id)
-      .then(() => {
-        breadcrumb.text("Message Sent");
-        $(this).trigger("reset");
-      })
-      .catch(() => {
-        breadcrumb.toggleClass("breadcrumb--failed");
-        breadcrumb.text(
-          "Sorry, we could't send your message. Please try again"
-        );
-      })
-      .finally(() => {
-        breadcrumb.show();
-        breadcrumb.toggleClass("breadcrumb--show");
-
-        form.forEach((f) => f.prop("disabled", false));
-
-        return new Promise((r) => setTimeout(r, 5000));
-      })
-      .then(() => {
-        breadcrumb.toggleClass(
-          ["breadcrumb--show", "breadcrumb--failed"],
-          false
-        );
-      });
-  });
+  $("#email-form").submit(contactFormSubmit);
+  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(smoothScroll);
 });
---
+++ src/scripts/contactFormSubmit.js
@@ -0,0 +1,52 @@
+import $ from "jquery";
+import emailjs from "emailjs-com";
+import config from "../../config/credentials.json";
+
+function contactFormSubmit(event) {
+  event.preventDefault();
+
+  const breadcrumb = $("#breadcrumb");
+  const user_name = $("#name");
+  const user_email = $("#email");
+  const message = $("#message");
+  const form = [user_name, user_email, message];
+
+  // SENDING STATUS
+  form.forEach((f) => f.prop("disabled", true));
+
+  const formData = {
+    user_name: user_name.val(),
+    user_email: user_email.val(),
+    message: message.val(),
+  };
+
+  emailjs
+    .send(config.service_id, config.template_id, formData, config.user_id)
+    .then(() => {
+      breadcrumb.text("Message Sent");
+      $(this).trigger("reset");
+    })
+    .catch(() => {
+      breadcrumb.toggleClass("breadcrumb--failed");
+      breadcrumb.text("Sorry, we could't send your message. Please try again");
+    })
+    .finally(() => {
+      breadcrumb.show();
+      breadcrumb.toggleClass("breadcrumb--show");
+
+      form.forEach((f) => f.prop("disabled", false));
+
+      return new Promise((r) => setTimeout(r, 5000));
+    })
+    .then(() => {
+      breadcrumb.toggleClass("breadcrumb--show", false);
+
+      // remove class after animation
+      setTimeout(
+        () => breadcrumb.toggleClass("breadcrumb--failed", false),
+        500
+      );
+    });
+}
+
+export default contactFormSubmit;
